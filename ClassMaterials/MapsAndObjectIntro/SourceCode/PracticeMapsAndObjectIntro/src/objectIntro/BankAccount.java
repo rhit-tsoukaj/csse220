63576d5502d7035497766271c4fc71c8dd22491f
rhit-tsoukaj <tsoukaj@rose-hulman.edu>
2026-09-14 09:18:09 -0400

    Update BankAccount.java

--- ClassMaterials/MapsAndObjectIntro/SourceCode/PracticeMapsAndObjectIntro/src/objectIntro/BankAccount.java
+++ ClassMaterials/MapsAndObjectIntro/SourceCode/PracticeMapsAndObjectIntro/src/objectIntro/BankAccount.java
@@ -2,4 +2,45 @@ package objectIntro;
 
 public class BankAccount {
 
+
+
+    private String name;
+    private double balance;
+
+    //Fields and Data
+    /**
+     * Constructs a default bank account.
+     */
+    public BankAccount() {
+        this.name = "UNKNOWN";
+        this.balance = 0.0;
+    }
+
+    //Methods
+    /**
+     * Constructs an account with the given name nad starting balance
+     * @param name the account name
+     * @param initialBalance starting account balance, in USD
+     */
+    public BankAccount(String name, double initialBalance) {
+    this.name = name;
+    this.balance = initialBalance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+
+    }
 }
